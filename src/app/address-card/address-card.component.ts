e4f52fa18e35e2e2ee6ec32881b93babb8995bac
hemme82 <davidhemmesch@gmail.com>
2019-11-11 15:32:55 -0600

    first commit

---
+++ src/app/address-card/address-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-address-card',
+  templateUrl: './address-card.component.html',
+  styleUrls: ['./address-card.component.css']
+})
+export class AddressCardComponent implements OnInit {
+
+  user: any;
+  
+  constructor() {
+    this.user = {
+     name: "Foo Bar!",
+     title: "Software Developer",
+     address: "1234 Main St, City, State, 100010",
+     phone: [
+       "123-123-1234",
+       "456-456-4567",
+       "789-789-7890"
+      ]
+    };
+   }
+
+  ngOnInit() {
+  }
+
+}
