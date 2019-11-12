b05da027d501bccdf19f9962685b314d49c9c278
hemme82 <davidhemmesch@gmail.com>
2019-11-12 10:30:45 -0600

    Passing member variables to components

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-address-card',
@@ -7,24 +8,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AddressCardComponent implements OnInit {
 
-  user: any;
-  @Input("name") userName: string;///input variable. "name" in parenthesis needs to be same name as app.component.html
+  
+  @Input("user") user: User;///input variable. "name" in parenthesis needs to be same name as app.component.html
   
   constructor() {
     
    }
 
   ngOnInit() {
-    this.user = {
-     name: this.userName,
-     title: "Software Developer",
-     address: "1234 Main St, City, State, 100010",
-     phone: [
-       "123-123-1234",
-       "456-456-4567",
-       "789-789-7890"
-      ]
-    };
+    }
   }
 
-}
+
